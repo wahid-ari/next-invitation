5c5d9e0b3f8e529d339f42f6f7336ea29e5290d3
wahid-ari <wahiid.arii@gmail.com>
2024-07-07 13:50:09 +0700

    update comment component

--- pages/comment.js
+++ pages/comment.js
@@ -35,7 +35,6 @@ export default function Comment() {
         come: parseInt(come),
       },
     ]);
-    console.log(error);
     if (!error) {
       setFetched(false);
       setName('');
@@ -46,31 +45,34 @@ export default function Comment() {
   }
 
   return (
-    <div className='relative bg-black'>
+    <div className='relative min-h-screen bg-black'>
       <div className='pointer-events-none absolute inset-x-0 bottom-0 top-0 z-0 flex justify-center'>
-        <div className='grid h-full min-h-screen w-full grid-cols-3 gap-3.5 px-4 sm:grid-cols-4'>
-          <div className='h-full w-full rounded-md bg-gradient-to-br from-sky-500/20 via-green-500/20 to-yellow-500/20 px-[0.5px]'>
+        <div className='grid h-full min-h-screen w-full grid-cols-3 gap-3.5 px-4 sm:grid-cols-5'>
+          <div className='h-full w-full rounded-md bg-gradient-to-br from-sky-600/20 via-green-600/20 to-yellow-600/20 px-[0.5px]'>
             <div className='flex h-full w-full items-center justify-center bg-black'></div>
           </div>
-          <div className='h-full w-full rounded-md bg-gradient-to-br from-yellow-500/20 via-blue-500/20 to-green-500/20 px-[0.5px]'>
+          <div className='h-full w-full rounded-md bg-gradient-to-br from-yellow-600/20 via-blue-600/20 to-green-600/20 px-[0.5px]'>
             <div className='flex h-full w-full items-center justify-center bg-black'></div>
           </div>
-          <div className='h-full w-full rounded-md bg-gradient-to-br from-green-500/20 via-yellow-500/20 to-blue-500/20 px-[0.5px]'>
+          <div className='h-full w-full rounded-md bg-gradient-to-br from-green-600/20 via-yellow-600/20 to-blue-600/20 px-[0.5px]'>
             <div className='flex h-full w-full items-center justify-center bg-black'></div>
           </div>
-          <div className='hidden h-full w-full rounded-md bg-gradient-to-br from-green-500/20 via-yellow-500/20 to-blue-500/20 px-[0.5px] sm:block'>
+          <div className='hidden h-full w-full rounded-md bg-gradient-to-br from-green-600/20 via-yellow-600/20 to-blue-600/20 px-[0.5px] sm:block'>
+            <div className='flex h-full w-full items-center justify-center bg-black'></div>
+          </div>
+          <div className='h-full w-full rounded-md bg-gradient-to-br from-yellow-600/20 via-blue-600/20 to-green-600/20 px-[0.5px]'>
             <div className='flex h-full w-full items-center justify-center bg-black'></div>
           </div>
         </div>
       </div>
       <div className='pointer-events-none absolute inset-x-0 z-0 flex justify-center'>
         <div className='grid h-full min-h-screen w-full grid-flow-col grid-rows-3 gap-3.5 py-4'>
-          <div className='border-b border-t border-b-green-500/20 border-t-yellow-500/20'></div>
-          <div className='border-b border-t border-b-yellow-500/20 border-t-sky-500/20'></div>
-          <div className='border-b border-t border-b-green-500/20 border-t-sky-500/20'></div>
+          <div className='border-b border-t border-b-green-600/20 border-t-yellow-600/20'></div>
+          <div className='border-b border-t border-b-yellow-600/20 border-t-sky-600/20'></div>
+          <div className='border-b border-t border-b-green-600/20 border-t-sky-600/20'></div>
         </div>
       </div>
-      <div className='relative z-[1] mx-auto max-w-xl items-center gap-1.5 p-8'>
+      <div className='relative z-[1] mx-auto max-w-xl items-center gap-1.5 p-4'>
         {/* <form>
         <div className="h-10 w-full rounded-md bg-gradient-to-r from-sky-500 via-green-500 to-yellow-500 p-0.5">
           <div className="flex h-full w-full items-center justify-center bg-transparent">
@@ -117,7 +119,7 @@ export default function Comment() {
           </div>
         ) : null} */}
 
-        <form className='mt-8' onSubmit={onSubmit}>
+        <form className='mt-' onSubmit={onSubmit}>
           <div className='rotate-border rotate-border-1 relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-transparent p-0.5'>
             <input
               value={name}
@@ -137,7 +139,7 @@ export default function Comment() {
               name='comment'
               placeholder='Comment'
               required
-              className='z-10 flex min-h-[100px] w-full rounded-md border border-neutral-600 bg-black px-3 py-2 text-sm placeholder:text-neutral-400 focus-visible:border-neutral-600 focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-1 focus-visible:ring-offset-neutral-900'
+              className='z-10 flex min-h-[80px] w-full rounded-md border border-neutral-600 bg-black px-3 py-2 text-sm placeholder:text-neutral-400 focus-visible:border-neutral-600 focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-1 focus-visible:ring-offset-neutral-900'
             />
           </div>
           <div className='rotate-border rotate-border-3 relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-transparent p-0.5'>
@@ -155,54 +157,140 @@ export default function Comment() {
               <option value='3'>not sure</option>
             </select>
           </div>
-          <div className='mt-4 h-8 w-20 rounded-md bg-gradient-to-r from-sky-500 to-green-500'>
-            <div className='flex h-full items-center justify-center bg-transparent'>
+          {/* <div className='mt-4 h-8 w-24 rounded-md bg-gradient-to-r from-sky-500 p-[0.5px] via-yellow-500 to-green-500'>
+            <div className='flex h-full items-center justify-center bg-black'>
               <button
                 disabled={isSubmiting}
                 type='submit'
                 name='submit'
-                className='text-sm font-medium disabled:cursor-not-allowed'
+                className='flex items-center gap-2 text-sm font-medium disabled:cursor-not-allowed'
               >
+                {isSubmiting &&
+                  <svg
+                    className='h-5 w-5 animate-spin text-white'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                  >
+                    <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+                    <path
+                      className='opacity-75'
+                      fill='currentColor'
+                      d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                    ></path>
+                  </svg>
+                }
                 Submit
               </button>
             </div>
-          </div>
+          </div> */}
+          <button
+            disabled={isSubmiting}
+            type='submit'
+            name='submit'
+            className='group relative mt-4 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-yellow-600 via-green-500 to-sky-500 p-0.5 text-sm font-medium text-white focus:outline-none focus-visible:ring-1 focus-visible:ring-green-500 focus-visible:ring-offset-1 focus-visible:ring-offset-black disabled:cursor-not-allowed group-hover:from-yellow-600 group-hover:via-green-600 group-hover:to-sky-500'
+          >
+            <span className='relative flex gap-2 rounded-md bg-black px-4 py-1.5 transition-all duration-75 ease-in group-hover:bg-opacity-0'>
+              {isSubmiting && (
+                <svg
+                  className='h-5 w-5 animate-spin text-white'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                  ></path>
+                </svg>
+              )}
+              Submit
+            </span>
+          </button>
         </form>
-        {fetched && commentData ? (
-          <div className='overflow-aut scrollbar-thumb-rounded mt-8 h-72 scrollbar-thin'>
-            {commentData.map((data, index) => (
-              <div key={data.id} className='mb-3'>
-                <p className='text-[17px] font-medium text-sky-500'>{data.name}</p>
-                {/* <p className='font-medium bg-gradient-to-r from-green-500 via-orange-500 to-sky-500 bg-clip-text text-transparent'>{data.name}</p> */}
-                <p className='text-neutral-200'>{data.comment}</p>
-                <p className='mt-1 flex items-center gap-1 text-[13px] text-neutral-400'>
-                  <ClockIcon className='h-4 w-4' />
-                  {moment(new Date(data.created_at)).fromNow(
-                    updateLocale('en', {
-                      relativeTime: {
-                        future: 'in %s',
-                        past: '%s ',
-                        s: 'sec',
-                        m: '%d menit lalu',
-                        mm: '%d menit lalu',
-                        h: '%d jam lalu',
-                        hh: '%d jam lalu',
-                        d: '%d hari lalu',
-                        dd: '%d hari lalu',
-                        M: 'a mth',
-                        MM: '%d mths',
-                        y: 'y',
-                        yy: '%d y',
-                      },
-                    }),
-                  )}
-                </p>
-                {/* {index < commentData.length - 1 && <div className='mt-4 border-b border-b-neutral-800' />} */}
-              </div>
-            ))}
-          </div>
-        ) : null}
+        {fetched && commentData.length > 0 ? (
+          <BorderFrame>
+            <div className='scrollbar-thumb-rounded mt-12 h-72 overflow-auto px-4 py-2 scrollbar-thin'>
+              {commentData.map((data, index) => (
+                <div key={data.id} className='mb-3'>
+                  <p className='text-[17px] font-medium text-sky-500'>{data.name}</p>
+                  {/* <p className='font-medium bg-gradient-to-r from-green-500 via-orange-500 to-sky-500 bg-clip-text text-transparent'>{data.name}</p> */}
+                  <p className='text-neutral-200'>{data.comment}</p>
+                  <p className='mt-1 flex items-center gap-1 text-[13px] text-neutral-400'>
+                    <ClockIcon className='h-4 w-4' />
+                    {moment(new Date(data.created_at)).fromNow(
+                      updateLocale('en', {
+                        relativeTime: {
+                          future: 'in %s',
+                          past: '%s ',
+                          s: 'sec',
+                          m: '%d menit lalu',
+                          mm: '%d menit lalu',
+                          h: '%d jam lalu',
+                          hh: '%d jam lalu',
+                          d: '%d hari lalu',
+                          dd: '%d hari lalu',
+                          M: 'a mth',
+                          MM: '%d mths',
+                          y: 'y',
+                          yy: '%d y',
+                        },
+                      }),
+                    )}
+                  </p>
+                  {/* {index < commentData.length - 1 && <div className='mt-4 border-b border-b-neutral-800' />} */}
+                </div>
+              ))}
+            </div>
+          </BorderFrame>
+        ) : fetched && commentData.length == 0 ? (
+          <BorderFrame>
+            <p className='mt-12 px-4 py-2'>No comment found.</p>
+          </BorderFrame>
+        ) : (
+          <BorderFrame>
+            <p className='mt-12 px-4 py-2'>Loading comment...</p>
+          </BorderFrame>
+        )}
       </div>
+    </div>
+  );
+}
+
+function BorderFrame({ children }) {
+  return (
+    <div className='relative'>
+      <div className='absolute -inset-x-0 top-0 h-px bg-gradient-to-r from-transparent via-yellow-500 to-transparent sm:-inset-x-12 md:-inset-x-16' />
+      <div
+        style={{
+          backgroundImage: `linear-gradient(to right, transparent, rgb(255 255 255 / 0.25) 24px, rgb(255 255 255 / 0.25) calc(100% - 24px), transparent)`,
+        }}
+        className='absolute -inset-x-0 -top-[15px] h-px sm:-inset-x-12 md:-inset-x-16'
+      />
+      <div className='absolute -inset-x-0 bottom-0 h-px bg-gradient-to-r from-transparent via-sky-500 to-transparent sm:-inset-x-12 md:-inset-x-16' />
+      <div
+        style={{
+          backgroundImage: `linear-gradient(to right, transparent, rgb(255 255 255 / 0.25) 24px, rgb(255 255 255 / 0.25) calc(100% - 24px), transparent)`,
+        }}
+        className='absolute -inset-x-0 -bottom-[15px] h-px sm:-inset-x-12 md:-inset-x-16'
+      />
+      <div className='absolute -inset-y-16 left-0 w-px bg-gradient-to-b from-transparent via-green-500 to-transparent' />
+      <div
+        style={{
+          backgroundImage: `linear-gradient(to bottom, transparent, rgb(255 255 255 / 0.25) 24px, rgb(255 255 255 / 0.25) calc(100% - 24px), transparent)`,
+        }}
+        className='absolute -inset-y-16 -left-[15px] w-px'
+      />
+      <div className='absolute -inset-y-16 right-0 w-px bg-gradient-to-b from-transparent via-orange-500 to-transparent' />
+      <div
+        style={{
+          backgroundImage: `linear-gradient(to bottom, transparent, rgb(255 255 255 / 0.25) 24px, rgb(255 255 255 / 0.25) calc(100% - 24px), transparent)`,
+        }}
+        className='absolute -inset-y-16 -right-[15px] w-px'
+      />
+      {children}
     </div>
   );
 }
