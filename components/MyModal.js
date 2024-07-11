fbfddc15d770ad8c456aade7e1a0d07a7e3ade41
wahid-ari <wahiid.arii@gmail.com>
2024-07-11 17:02:33 +0700

    using new app route

--- components/MyModal.js
+++ components/MyModal.js
@@ -42,7 +42,7 @@ export default function MyModal({ isOpen, closeModal }) {
                 <div className='onboard flex items-center justify-center'>
                   <Gradient width={1000} height={700} className='top-[-200px] opacity-20' conic />
                   <div className='relative flex h-[200px] w-full items-center justify-center'>
-                    <div className='absolute z-10 min-h-[800px] min-w-[800px]'>
+                    <div className='absolute z-10 min-h-[650px] min-w-[650px] sm:min-h-[800px] sm:min-w-[800px]'>
                       <Image
                         alt='Turborepo'
                         src='/pack-hexagons.svg'
@@ -57,7 +57,7 @@ export default function MyModal({ isOpen, closeModal }) {
                       <h1
                         className={cn(
                           merienda.className,
-                          'mb-2 mt-8 bg-gradient-to-b from-white to-[#AAAAAA] bg-clip-text text-5xl font-bold text-transparent text-white',
+                          'mb-2 mt-8 bg-gradient-to-b from-white to-[#AAAAAA] bg-clip-text text-4xl font-bold text-transparent text-white sm:text-5xl',
                         )}
                       >
                         {/* {config.man} & {config.woman} */}
@@ -65,7 +65,7 @@ export default function MyModal({ isOpen, closeModal }) {
                         <span
                           className={cn(
                             merienda.className,
-                            'bg-gradient-to-r from-yellow-700 via-green-500 to-sky-500 bg-clip-text text-5xl font-extrabold text-transparent md:text-center',
+                            'bg-gradient-to-r from-yellow-700 via-green-500 to-sky-500 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl md:text-center',
                           )}
                         >
                           &
@@ -74,11 +74,11 @@ export default function MyModal({ isOpen, closeModal }) {
                       </h1>
                       {/* <h3 className="text-5xl font-bold text-white mb-2 mt-8 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-green-600 to-sky-500">John & Jane</h3> */}
 
-                      <Border className='mx-auto' />
+                      <Border className='mx-auto w-56 sm:w-auto' />
 
-                      <h2 className='mb-2 mt-4 text-xl font-semibold text-white'>{config.date}</h2>
-                      <h2 className='text-xl font-semibold text-white'>{config.location}</h2>
-                      <div className='mt-8'>
+                      <h2 className='mb-2 mt-2 text-lg font-semibold text-white sm:mt-4 sm:text-xl'>{config.date}</h2>
+                      <h2 className='text-lg font-semibold text-white sm:text-xl'>{config.location}</h2>
+                      <div className='mt-4 sm:mt-8'>
                         <button
                           onClick={closeModal}
                           className='group relative mb-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-yellow-600 via-green-500 to-sky-500 p-0.5 text-sm font-medium text-white focus:outline-none focus-visible:ring-1 focus-visible:ring-1 focus-visible:ring-green-500 focus-visible:ring-offset-1 focus-visible:ring-offset-black group-hover:from-yellow-600 group-hover:via-green-600 group-hover:to-sky-500'
